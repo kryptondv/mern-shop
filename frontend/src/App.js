44ef5a21c7bc42c20671c942453d0d6df4eec23d
kryptondv <kryptondevpl@gmail.com>
2021-10-02 10:43:37 +0200

    added header and footer components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,12 @@
+import Footer from './components/Footer';
+import Header from './components/Header';
+
 const App = () => {
     return (
         <>
+            <Header />
             <h1>Welcome to Proshop</h1>
+            <Footer />
         </>
     );
 };
