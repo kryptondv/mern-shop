49ccd1bf8329e65e510ca950f80e786f43ff624b
kryptondv <kryptondevpl@gmail.com>
2021-10-07 20:54:15 +0200

    refactored rating component

--- frontend/src/components/Rating.js
+++ frontend/src/components/Rating.js
@@ -1,28 +1,29 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { FaStar, FaStarHalfAlt, FaRegStar } from 'react-icons/fa';
 
-const Rating = ({ val, text }) => {
+const Rating = ({ val, text, color = '#f8e825' }) => {
     const renderStars = () => {
-        const stars = [];
-        for (let i = 1; i <= val; i++) {
-            stars.push('full');
-        }
-        if (val % 1 > 0) stars.push('half');
-        for (let i = 1; i < 5 - stars.length; i++) {
-            stars.push('empty');
-        }
-        return stars.map((star, i) => (
-            <React.Fragment key={i}>
-                {star === 'full' ? <FaStar /> : star === 'half' ? <FaStarHalfAlt /> : <FaRegStar />}
-            </React.Fragment>
-        ));
+        return [...Array(5)].map((item, i) => {
+            return (
+                <i key={i} style={{ color }}>
+                    {val >= i + 1 ? <FaStar /> : val > i ? <FaStarHalfAlt /> : <FaRegStar />}
+                </i>
+            );
+        });
     };
 
     return (
-        <div className="rating">
-            {renderStars()} of {text}
+        <div className="rating my-2">
+            {renderStars()} of {text && text}
         </div>
     );
+};
+
+Rating.propTypes = {
+    val: PropTypes.number.isRequired,
+    text: PropTypes.string,
+    color: PropTypes.string,
 };
 
 export default Rating;
