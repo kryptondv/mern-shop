e6e7ab0e456a6f047d345da205bab3e9266b084e
kryptondv <kryptondevpl@gmail.com>
2021-10-26 18:33:45 +0200

    moved product routes logic to productController

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,6 +1,5 @@
 import express from 'express';
-import asyncHandler from 'express-async-handler';
-import Product from '../models/product.model.js';
+import { getProducts, getProductById } from '../controllers/productController.js';
 
 const router = express.Router();
 
@@ -8,29 +7,12 @@ const router = express.Router();
 Fetch all products
 GET /api/products
 */
-router.get(
-    '/',
-    asyncHandler(async (req, res) => {
-        const products = await Product.find({});
-        res.json(products);
-    })
-);
+router.route('/').get(getProducts);
 
 /*
 Fetch single product
 GET /api/products/:id
 */
-router.get(
-    '/:id',
-    asyncHandler(async (req, res) => {
-        const product = await Product.findById(req.params.id);
-        if (product) {
-            res.json(product);
-        } else {
-            res.status(404);
-            throw new Error('Product not found');
-        }
-    })
-);
+router.route('/:id').get(getProductById);
 
 export default router;
